e8e3ad1ac29ee3a523ccf6b543dde9bc9aea568d
Juan Quintana <juan.quintana5@upr.edu>
2024-05-19 03:46:20 -0400

    Started NavBar

---
+++ components/Navbar.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React, { use } from "react";
+import { useState, useEffect } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Button,
+  Link,
+} from "@nextui-org/react";
+
+export default function NavBar() {
+  const [screenWidth, setWidth] = useState(window.innerWidth);
+  function handleResize(width: number) {
+    setWidth(width);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", () => handleResize(window.innerWidth));
+  });
+  if (screenWidth < 768) {
+    return (
+      <Navbar>
+        <NavbarBrand>
+          <h2 className="font-sans italic font-bold text-xl">
+            Hi I'm <span className="text-blue-700">Juan Quintana!</span>
+          </h2>
+        </NavbarBrand>
+
+        <NavbarMenuToggle />
+        <NavbarMenu>
+          <NavbarMenuItem>About</NavbarMenuItem>
+          <NavbarMenuItem>Projects</NavbarMenuItem>
+          <NavbarMenuItem>Contact</NavbarMenuItem>
+        </NavbarMenu>
+      </Navbar>
+    );
+  } else {
+    return (
+      <Navbar className="">
+        <NavbarBrand>
+          <h2 className="font-sans italic font-bold text-xl">
+            Hi I'm <span className="text-blue-700">Juan Quintana!</span>
+          </h2>
+        </NavbarBrand>
+        <NavbarContent>
+          <NavbarItem>
+            <NavbarMenuItem>About</NavbarMenuItem>
+          </NavbarItem>
+          <NavbarItem>
+            <NavbarMenuItem>Projects</NavbarMenuItem>
+          </NavbarItem>
+          <NavbarItem>
+            <NavbarMenuItem>Contact</NavbarMenuItem>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent>
+          <NavbarItem>
+            <NavbarItem>
+              <Button>My Resume</Button>
+            </NavbarItem>
+          </NavbarItem>
+          <NavbarItem>
+            <NavbarItem>
+              <Button as={Link}>MyResume</Button>
+            </NavbarItem>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    );
+  }
+}

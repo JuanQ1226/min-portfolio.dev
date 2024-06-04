32edd6e16b833610423174229f0ddedfbadf7c47
Juan Quintana <juan.quintana5@upr.edu>
2024-06-04 14:46:11 -0400

    Added Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import AboutMe from "@/components/AboutMe";
 import Hero from "../components/Hero";
 import Experiences from "@/components/Experience";
 import Education from "@/components/Education";
+import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
     <main className="overflow-clip">
+      <Analytics />
       <Hero />
       <AboutMe />
       <Experiences />

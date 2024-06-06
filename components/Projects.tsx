2b9efed807e944ef4ff02eab5de94497074957b2
Juan Quintana <juan.quintana5@upr.edu>
2024-06-06 00:22:29 -0400

    Added Projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,6 +9,31 @@ export default function Projects() {
         <div className="text-2xl font-semibold italic">
           My Projects<span className="text-primary">.</span>
         </div>
+
+        <div className="grid grid-cols-2 items-center gap-5">
+          <Image
+            alt="maze_generator Image"
+            height={400}
+            width={400}
+            src={"./briq.png"}
+            className="shadow-md shadow-gray-500 hover:scale-105 transform transition duration-300 ease-in-out"
+          />
+          <div>
+            <h2 className="text-lg font-semibold">
+              Briq (E-commerce for construction materials in PR).
+            </h2>
+            <div className="text-sm">
+              This is a product I&apos;m currently designing and developing with
+              Stackz. This platform will allow construction material providers
+              in PR to have a centralized portal to post their products. This
+              will allow contractors to easily view and purchase the products
+              that providers have available. The platform is a web app
+              implemented in Next.js, Postgresql and uses Vercel for data
+              storage and hosting services.
+            </div>
+          </div>
+        </div>
+        <Divider className="my-4" />
         <div className="grid grid-cols-2 items-center  gap-2">
           <div>
             <Link
@@ -38,30 +63,6 @@ export default function Projects() {
             width={400}
             src={"./maze_generator.png"}
           />
-        </div>
-        <Divider className="my-4" />
-        <div className="grid grid-cols-2 items-center gap-5">
-          <Image
-            alt="maze_generator Image"
-            height={400}
-            width={400}
-            src={"./maze_generator.png"}
-            className="shadow-md shadow-gray-500 hover:scale-105 transform transition duration-300 ease-in-out"
-          />
-          <div>
-            <h2 className="text-lg font-semibold">
-              Briq (E-commerce for construction materials in PR).
-            </h2>
-            <div className="text-sm">
-              This is a product I&apos;m currently designing and developing with
-              Stackz. This platform will allow construction material providers
-              in PR to have a centralized portal to post their products. This
-              will allow contractors to easily view and purchase the products
-              that providers have available. The platform is a web app
-              implemented in Next.js, Postgresql and uses Vercel for data
-              storage and hosting services.
-            </div>
-          </div>
         </div>
       </div>
     </section>
